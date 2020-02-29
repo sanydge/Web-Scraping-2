32a33b5b6561a28d6f72a45c213b21bfb3407f7c
Tirdea Alexandru <sanydge@gmail.com>
2020-02-29 12:01:03 +0200

    Scraping Geekbuying(part1)

---
+++ geekbuying/spiders/deals.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class DealsSpider(scrapy.Spider):
+    name = 'deals'
+    allowed_domains = ['www.geekbuying.com']
+    start_urls = ['https://www.geekbuying.com/deals']
+
+    def parse(self, response):
+        products = response.xpath("//div[@class='category_li']")
+        for product in products:
+            product_name = product.xpath(".//a[@class='category_li_link']/text()").get()
+            product_url = product.xpath(".//a[@class='category_li_link']/@href").get()
+            product_price = product.xpath(".//div[@class='category_li_price']/span/text()").get()
+            promotion_ends = product.xpath(".//div[@class = 'category_li_claibg']/span/text()").get()
+            yield {
+                'name': product_name,
+                'url': product_url,
+                'price': product_price,
+                'promotion': promotion_ends
+            }
